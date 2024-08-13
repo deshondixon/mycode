256f20a718fa853fb1a6fe0436198a7d54f4ecbb
DeShon Dixon <deshondixon@gmail.com>
2024-08-13 15:46:56 +0000

    mixed lists

---
+++ bustedcode.py
@@ -0,0 +1,8 @@
+#!/usr/env python3
+
+def calculator():
+   num1= int(input("Enter your first number: "))
+   num2= int(input("Enter your second number: "))
+   print(f"{num1} + {num2} = {num1 + num2}")
+
+calculator()   
